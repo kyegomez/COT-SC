8b9c81e99b8c036daa513ba0cc98a41de2a04893
Kye <kye@apacmediasolutions.com>
2023-05-29 22:59:32 -0400

    self consitency

--- sc.py
+++ sc.py
@@ -1,6 +1,6 @@
 import random
 from collections import Counter
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 def sample_outputs(prompt: str, question: str, language_model, num_samples: int) -> List[Tuple[str, str]]:
     sampled_outputs = []
@@ -9,6 +9,40 @@ def sample_outputs(prompt: str, question: str, language_model, num_samples: int)
         reasoning_path, answer = parse_output(output)
         sampled_outputs.append((reasoning_path, answer))
     return sampled_outputs
+
+
+def get_rejected_reason(sampled_outputs: List[Tuple[str, str]]) -> Dict[str, Any]:
+    rejection_reasons = {}
+    for output in sampled_outputs:
+        reasoning_paths, answer = output
+        reason = check_rejection_reason(reasoning_paths, answer)
+        if reason:
+            rejection_reasons[reasoning_path] = reason
+    return rejection_reasons
+
+def check_rejection_reason(reasoning_path, answer: str) -> Optional[Any]:
+    #implement an function to check the reasons for rejections
+    pass
+
+
+def adjusted_outputs(sampled_outputs: List[Tuple[str, str]], rejection_reasoning: Dict[str, Any]) -> List[Tuple[str, str]]:
+    adjusted_outputs = []
+    for output in sampled_outputs:
+        reasoning_paths, answer = output
+        if reasoning_paths not in rejection_reasonings:
+            adjusted_outputs.append(output)
+        else:
+            adjusted_outputs_apth = adjust_reasoning_path(reasoning_path, rejection_reasons)
+            adjusted_outputs.append((adjusted_reasoning_path, answer))
+    return adjusted_outputs
+
+def adjust_reasoning_path(reasoning_path: str, rejection_reasons: Dict[str, Any]) -> str:
+    #implement function to adjust the reasoning reasoning path based on the rehected reasons 
+    pass
+
+
+
+
 
 def parse_output(output: str) -> Tuple[str, str]:
     # Implement a function to parse the output into reasoning_path and answer
@@ -25,6 +59,8 @@ def find_most_consistent_answer(aggregated_answers: List[str]) -> str:
 
 def self_consistency(prompt: str, question: str, language_model, num_samples: int) -> str:
     sampled_outputs = sample_outputs(prompt, question, language_model, num_samples)
-    aggregated_answers = aggregate_answers(sampled_outputs)
+    rejection_reasons = get_rejection_reasons(sampled_outputs)
+    adjusted_outputs = adjust_outputs(sampled_outputs, rejection_reasons)
+    aggregated_answers = aggregate_answers(adjusted_outputs)
     most_consistent_answer = find_most_consistent_answer(aggregated_answers)
     return most_consistent_answer
